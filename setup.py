b79cddb622bda8ae6921b7266e82654ad9cc5cbd
Brennen Bearnes <bbearnes@gmail.com>
2018-08-14 21:54:35 -0600

    1.6.2: adding deprecation notice to readme on pypi

--- setup.py
+++ setup.py
@@ -6,10 +6,19 @@ except ImportError:
     # Ignore import error and assume Python 3 which already has setuptools.
     pass
 
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 from setuptools import setup, find_packages
 
 setup(name              = 'Adafruit_TMP',
-      version           = '1.6.1',
+      version           = '1.6.2',
       author            = 'Tony DiCola',
       author_email      = 'tdicola@adafruit.com',
       description       = 'Library for accessing the TMP006 & TMP007  non-contact temperature sensor on a Raspberry Pi or Beaglebone Black.',
@@ -17,4 +26,7 @@ setup(name              = 'Adafruit_TMP',
       url               = 'https://github.com/adafruit/Adafruit_Python_TMP/',
       dependency_links  = ['https://github.com/adafruit/Adafruit_Python_GPIO/tarball/master#egg=Adafruit-GPIO-0.6.5'],
       install_requires  = ['Adafruit-GPIO>=0.6.5'],
-      packages          = find_packages())
+      packages          = find_packages(),
+
+      long_description = long_description,
+      long_description_content_type = 'text/markdown')
